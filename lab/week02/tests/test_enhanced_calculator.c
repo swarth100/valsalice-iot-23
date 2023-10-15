1cf57ffa6f45032d4cf2ffb8c7bc063eaac84d21
Alberto Spina <alberto.spina.1996@gmail.com>
2023-10-15 16:07:53 +0100

    Add lab/week02/tests/test_enhanced_calculator.c

---
+++ lab/week02/tests/test_enhanced_calculator.c
@@ -0,0 +1,24 @@
+#include "unity.h"
+#include "enhanced_calculator.c"
+
+void setUp() {}
+
+void tearDown() {}
+
+void test_performCalculation() {
+    struct Calculation calc1 = {'+', 5, 3};
+    struct Calculation calc2 = {'-', 5, 3};
+    struct Calculation calc3 = {'*', 5, 3};
+    struct Calculation calc4 = {'/', 6, 2};
+
+    TEST_ASSERT_EQUAL(8, performCalculation(calc1));
+    TEST_ASSERT_EQUAL(2, performCalculation(calc2));
+    TEST_ASSERT_EQUAL(15, performCalculation(calc3));
+    TEST_ASSERT_EQUAL(3, performCalculation(calc4));
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_performCalculation);
+    return UNITY_END();
+}
