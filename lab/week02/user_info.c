54434e3a782776e3922c4b4598a6afabc9f2933c
aspina <aspina@example.com>
2023-10-21 16:31:58 +0200

    Add additional input examples and files

---
+++ lab/week02/user_info.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    int age;
+    float height;
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+
+    printf("%s has age %d is %.2f meters tall.\n", name, age, height);
+
+    return 0;
+}
