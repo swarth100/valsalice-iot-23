17f017f634eaa3a21d245496ae96eb7fb81d5e9d
aspina <aspina@example.com>
2023-10-23 16:10:16 +0200

    Add hello_input.c logic

--- examples/6tisch/simple-node/node.c
+++ examples/6tisch/simple-node/node.c
@@ -56,24 +56,28 @@ PROCESS_THREAD(node_process, ev, data)
 {
   PROCESS_BEGIN();
 #if CONTIKI_TARGET_COOJA || CONTIKI_TARGET_Z1
-  if(node_id == 1) { /* Coordinator node. */
+  if (node_id == 1)
+  { /* Coordinator node. */
     NETSTACK_ROUTING.root_start();
   }
 #endif
   NETSTACK_MAC.on();
 
+#define WITH_PERIODIC_ROUTES_PRINT 1
+
 #if WITH_PERIODIC_ROUTES_PRINT
   static struct etimer et;
   /* Print out routing tables every minute */
   etimer_set(&et, CLOCK_SECOND * 60);
-  while(1) {
-    /* Used for non-regression testing */
-    #if (UIP_MAX_ROUTES != 0)
-      PRINTF("Routing entries: %u\n", uip_ds6_route_num_routes());
-    #endif
-    #if (UIP_SR_LINK_NUM != 0)
-      PRINTF("Routing links: %u\n", uip_sr_num_nodes());
-    #endif
+  while (1)
+  {
+/* Used for non-regression testing */
+#if (UIP_MAX_ROUTES != 0)
+    PRINTF("Routing entries: %u\n", uip_ds6_route_num_routes());
+#endif
+#if (UIP_SR_LINK_NUM != 0)
+    PRINTF("Routing links: %u\n", uip_sr_num_nodes());
+#endif
     PROCESS_YIELD_UNTIL(etimer_expired(&et));
     etimer_reset(&et);
   }
